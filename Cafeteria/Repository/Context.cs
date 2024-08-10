39943a6386ae266b7837076ac875f7d3ad2d326a
KaikAp <kaik.silva4@outlook.com>
2024-08-09 22:51:08 -0300

    Add Repository

---
+++ Cafeteria/Repository/Context.cs
@@ -0,0 +1,22 @@
+﻿using Business;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options): base(options) {}
+
+        public DbSet<Contato> Contatos { get; set; }
+
+        public DbSet<Estrutura> Estruturas { get; set; }
+
+        public DbSet<Categoria> Categorias { get; set; }
+
+        public DbSet<Alimento> Alimentos  { get; set; }
+
+        public DbSet<Variacao> Variacaos { get; set; }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
