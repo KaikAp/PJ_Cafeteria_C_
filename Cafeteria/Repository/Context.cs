cf6696b097b345f19f0ca374057ee3e7b2780752
KaikAp <kaik.silva4@outlook.com>
2024-08-11 00:04:10 -0300

    criando conexão com banco de dados

--- Cafeteria/Repository/Context.cs
+++ Cafeteria/Repository/Context.cs
@@ -18,5 +18,48 @@ namespace Repository
         public DbSet<Variacao> Variacaos { get; set; }
 
         public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //mapeamento das tabelas no sql
+            modelBuilder.Entity<Categoria>(
+                t =>
+                {
+                    t.ToTable("Categorias");
+                    t.Property(t => t.Id).HasColumnType("int").IsRequired().ValueGeneratedOnAdd();
+                    t.Property(t => t.Nome).HasColumnType("varchar(50)").IsRequired();
+                    t.HasKey(t => t.Id);
+                }
+                );
+
+            modelBuilder.Entity<Alimento>(
+                t =>
+                {
+                    t.ToTable("Alimentos");
+                    t.HasKey(t => t.Id);
+                    t.Property(t => t.Id).HasColumnType("int").IsRequired().ValueGeneratedOnAdd();
+                    t.Property(t => t.Descricao).HasColumnType("varchar(200)").IsRequired();
+                    t.Property(t => t.Nome).HasColumnType("varchar(50)").IsRequired();
+                    t.Property(t => t.Valor).HasColumnType("double").IsRequired();
+                    t.HasOne(t => t.Categoria).WithMany(t => t.alimentos).OnDelete(DeleteBehavior.NoAction).IsRequired();
+                }
+
+
+                );
+
+
+
+            modelBuilder.Entity<Contato>(
+                t =>
+                {
+                    t.ToTable("Contatos");
+                    t.HasKey(t=> t.Id);
+                    t.Property(t => t.Id).HasColumnType("int").IsRequired().ValueGeneratedOnAdd();
+                    t.Property(t => t.Email).HasColumnType("varchar(100)").IsRequired();
+                    t.Property(t => t.Celular).HasColumnType("varchar(20)").IsRequired();
+                }
+             );
+
+        }
     }
 }
